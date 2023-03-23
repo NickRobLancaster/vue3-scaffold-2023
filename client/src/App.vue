87c8de215978dedecbdd7411264a9ab1b5f4b9fc
SkywaveNick <92408834+SkywaveNick@users.noreply.github.com>
2023-03-23 01:33:28 -0700

    first commit after project deleted

---
+++ client/src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+/* add fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+import main from "./components/mainPage.vue";
+</script>
+
+<template>
+  <main />
+</template>
